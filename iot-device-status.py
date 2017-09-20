18a13dd3faa3a9fdb119edd4af554088988d0303
Daniel Alvarez-Torres <dalvarez@ec.ibm.com>
2017-09-20 04:35:42 +0000

    Add command callback

--- iot-device-status.py
+++ iot-device-status.py
@@ -7,15 +7,35 @@ from datetime import datetime
 
 #The following file must be updated before running this program
 configFilePath='./device.cfg'
+waitInterval=1
 
 def get_system_load():
 	load = json.loads(os.popen('./sys-stats.sh').read())
 	load['timestamp'] = datetime.now().isoformat()
 	return load
 
+	
+def command_callback(cmd):
+	print("Command received: %s" % cmd.data)
+	if cmd.command == "set_interval":
+		if 'interval' not in cmd.data:
+			print("Error: Interval was not present")
+		else:
+			waitInterval = cmd.data['interval']
+	elif cmd.command == "print":
+		if 'message' not in cmd.data:
+			print("Error: message was not included in command")
+		else:
+			print("Message Received from Watson IoT: %s" % cmd.data['message'])
+	elif cmd.command == "stop":
+		os.exit(0)
+	else:
+		print("Error: Unrecognized command: %s" % cmd.command)
+
 try:
   options = ibmiotf.device.ParseConfigFile(configFilePath)
   client = ibmiotf.device.Client(options)
+  client.commandCallback = command_callback
 except ibmiotf.ConnectionException  as e:
 	print "Error connecting to Watson IoT", e
 	os.exit(1)
@@ -24,5 +44,5 @@ client.connect()
 while True:
 	data = get_system_load()
 	client.publishEvent("status", "json", data)
-	time.sleep(1)
+	time.sleep(waitInterval)
 
